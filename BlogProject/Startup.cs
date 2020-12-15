4965d0787229a3f244cc2427d5ecccdad5048acf
patryk-pl <patrykbogdanski@outlook.com>
2020-12-15 13:30:22 +0100

    Create filders, files and add basics configuration
    Add _ViewStart, Shared/_Layout (Razor Layout) and Customer Area with HomeController. Add configuration in Startup file

--- BlogProject/Startup.cs
+++ BlogProject/Startup.cs
@@ -16,6 +16,8 @@ namespace BlogProject
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,10 +32,9 @@ namespace BlogProject
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                    name:"default",
+                    pattern:"{area=Customer}/{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
