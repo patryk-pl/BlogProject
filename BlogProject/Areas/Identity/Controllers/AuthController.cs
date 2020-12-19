50702dd9490d305aa67781cf69cc3357f6bd4800
patryk-pl <patrykbogdanski@outlook.com>
2020-12-19 17:44:39 +0100

    Add Login and logout methods to AuthController
    Add almost only signatures without logic

---
+++ BlogProject/Areas/Identity/Controllers/AuthController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogProject.Areas.Identity.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        public AuthController(SignInManager<IdentityUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View(new LoginViewModel());
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel loginVm)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Logout()
+        {
+            return View();
+        }
+    }
+}
