8ab76f627e42e09af88d921ce9994e161c6545c0
patryk-pl <patrykbogdanski@outlook.com>
2020-12-19 14:53:17 +0100

    Create Admin Role in program.cs

--- BlogProject/Program.cs
+++ BlogProject/Program.cs
@@ -2,8 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BlogProject.Database;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -13,7 +16,36 @@ namespace BlogProject
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            var scope = host.Services.CreateScope();
+
+            var ctx = scope.ServiceProvider.GetRequiredService<BlogProjectDbContext>();
+            var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+            var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+            ctx.Database.EnsureCreated();
+
+            var adminRole = new IdentityRole("Admin");
+            if (!ctx.Roles.Any())
+            {
+                roleManager.CreateAsync(adminRole).GetAwaiter().GetResult();
+                //Create a role
+            }
+
+            if (!ctx.Users.Any(u => u.UserName == "admin"))
+            {
+                //Create an admin
+                var adminUser = new IdentityUser
+                {
+
+                    UserName = "admin",
+                    Email = "admin@text.com"
+                };
+                userManager.CreateAsync(adminUser,"password");
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
